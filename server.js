0a2e4b10c7ad16264742f55f1285857a7b109475
Payal <pihuuprajapat4444@gmail.com>
2026-06-18 17:42:08 +0530

    add

--- server.js
+++ server.js
@@ -11,15 +11,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+app.get("/",(req, res)=>{
+    return res.send("backend is running")
+})
+
+let isConnected = false;
+async function connectToMongoDb(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifielsTopology: true
+        });
+        isConnected = true;
+        console.log("Connected to MongoDB");
+    }catch(error){
+        console.log('Error connecting to MongoDB', error)
+    }
+}
+
+ app.use((req, res, next)=>{
+    if(!isConnected){
+        connectToMongoDb();
+    }
+    next();
+})
 
 app.use("/api/auth", authRoutes);
 app.use("/api/slots", slotRoutes);
 app.use("/api/bookings", bookingRoutes);
 
-app.get("/",(req, res)=>{
-   return res.send("backend is running")
-})
+// const PORT = process.env.PORT
+// app.listen(() => console.log("Server running"));
 
-const PORT = process.env.PORT
-app.listen(() => console.log("Server running"));
+export default app
