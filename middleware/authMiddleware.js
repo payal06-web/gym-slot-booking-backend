dde6c4c5f5df50a44c97e88e11da20033640881f
Payal <pihuuprajapat4444@gmail.com>
2026-06-21 16:27:37 +0530

    Add

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,11 +7,10 @@ export const protect = (req, res, next) => {
     return res.status(401).json({ msg: "No token" });
   }
 
-  const token = authHeader.split(" ")[1]; // 🔥 FIX
+  const token = authHeader.split(" ")[1]; 
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     req.user = decoded;
     next();
   } catch {
