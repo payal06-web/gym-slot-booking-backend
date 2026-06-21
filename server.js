11e277a35e16dee2efa2c2c1b5ff44bbc7541230
Payal <pihuuprajapat4444@gmail.com>
2026-06-21 14:40:58 +0530

    add

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
 
-app.use(cors());
+app.use(cors({
+  origin: "https://gym-slot-booking-frontend.onrender.com",
+  credentials: true
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/slots", slotRoutes);
 app.use("/api/bookings", bookingRoutes);
